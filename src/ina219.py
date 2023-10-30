9eecde5ff4ea58fc242819392145b8f4aee903f1
SteveMayze <smayze@yahoo.com>
2023-10-30 14:30:22 +0100

    Script adjustments to allow for existing system constraints

--- src/ina219.py
+++ src/ina219.py
@@ -15,6 +15,7 @@ print(f"system voltage: {max_system_voltage}, load: {max_load}, full scale volta
 
 r_shunt = math.ceil((max_fsv_voltage / max_load)*1000)/1000
 ## r_shunt = (max_fsv_voltage / max_load)
+r_shunt = float(input(f"Shunt Resistance (correction for the existing sysem) [{r_shunt}]: ") or r_shunt)
 
 fs_shunt_current = max_fsv_voltage / r_shunt 
 
@@ -29,30 +30,11 @@ max_current_lsb = max_load / 2**12
 print (f"min current_lsb: {math.ceil(min_current_lsb*10**6)}µA, max current_lsb: {math.ceil(max_current_lsb*10**6)}µA")
 
 current_lsb = math.ceil(min_current_lsb*10000)/10000
+current_lsb = float(input(f"Preferred Current LSB [{current_lsb}]: ") or current_lsb)
 
 print(f"current_lsb: {current_lsb*10**6}µA")
 
 calibration = math.trunc(0.04096/(current_lsb * r_shunt))
 print(f"Calibration: {calibration} → {hex(calibration)}")
 print(f"\n======================================\n")
-
-print(f"\n========        Example       ========\n\n")
-
-## Shunt Voltage
-bus_voltage_plus = float(input(f"Bus Voltage +ve [{max_system_voltage}]: ") or max_system_voltage)
-load_current = float(input(f"Load current [{max_load}]: ") or max_load)
-
-r_shunt_vd = load_current * r_shunt
-
-print(f"Shunt resistor voltage drop: {r_shunt_vd} V")
-bus_voltage_neg = bus_voltage_plus - r_shunt_vd
-
-print(f"Bus Voltage -ve {bus_voltage_neg}")
-
-bus_v_reg = int(bus_voltage_neg * 100)<<3
-
-print(f"Bus Voltage Register {hex(bus_v_reg)}")
-## Bus Voltge
-## Current
-## Power
 
